eabfa3ef6e33b09bfda36766b3b9fedbef2cf8ba
FreakishLancer <destfinis@gmail.com>
2017-02-07 14:49:10 -0500

    Completed reset button

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
-let breakTimer = 5;
-let focusTimer = 25;
+let breakTimer;
+let focusTimer;
 
 function print(timer, $selector) {
   timer < 10 ?
@@ -20,9 +20,14 @@ function reset() {
   print(focusTimer, "#focus-timer");
   printFocusTimer(focusTimer);
   $("#focus-setter-container button").prop("disabled", false);
+  $("#start").prop("disabled", false);
+  $("#pause").prop("disabled", true).text("Pause").css("background", "#fff195");
+  $("#timer").css("color", "lightgray");
+  $("#focus-timer").css("color", "black");
 }
 
 $(document).ready(() => {
+  reset();
 
   $("#break-setter-container button.sub").on("click", () => {
     if (breakTimer === 1) return;
@@ -52,40 +57,55 @@ $(document).ready(() => {
     printFocusTimer(focusTimer);
   });
 
-  $("#start").on("click", () => {
+  $("#start").on("click", button => {
+    $("#start").prop("disabled", true);
+    $("#pause").prop("disabled", false);
     $("#focus-setter-container button").prop("disabled", true);
     $("#timer").css("color", "black");
     $("#focus-timer").css("color", "lightgray");
-
-    let seconds = 60;
     focusTimer--;
+    let seconds = 60;
+    let focusCounter = setInterval(timer, 1);
 
-    let focusCounter = setInterval(function() {
+    function timer() {
+      let minutesString = focusTimer.toString();
+      let secondsString = seconds.toString();
       seconds--;
+
       if (seconds === 0) {
         focusTimer--;
         seconds = 60;
       }
-      if (seconds === 60 && focusTimer > 9) {
-        $("#timer").text(`${focusTimer}:00`);
-      }
-      else if (seconds < 10 && focusTimer > 9) {
-        $("#timer").text(`${focusTimer}:0${seconds}`);
-      }
-      else if (seconds > 9 && focusTimer > 9) {
-        $("#timer").text(`${focusTimer}:${seconds}`);
-      }
-      else if (seconds < 10 && focusTimer < 10) {
-        $("#timer").text(`0${focusTimer}:0${seconds}`);
-      }
-      else if (seconds > 9 && focusTimer < 10) {
-        $("#timer").text(`0${focusTimer}:${seconds}`);
-      }
-    }, 1);
 
-    if (focusTimer === 20 && seconds === 40) {
+      focusTimer <= 9 ?
+        minutesString = "0" + focusTimer.toString() :
+        minutesString = focusTimer.toString();
+
+      seconds <= 9 ?
+        secondsString = "0" + seconds.toString() :
+        secondsString = seconds.toString();
+
+      if (seconds === 60) secondsString = "00";
+
+      if (minutesString === "00" && secondsString === "00") {
         clearInterval(focusCounter);
       }
+
+      $("#reset").on("click", () => {
+        clearInterval(focusCounter);
+      });
+
+      $("#timer").text(`${minutesString}:${secondsString}`);
+    }
+  });
+
+  $("#pause").on("click", button => {
+    if ($("#pause").text() === "Pause") {
+      $("#pause").text("Resume").css("background", "white");
+    }
+    else {
+      $("#pause").text("Pause").css("background", "#fff195");
+    }
   });
 
   $("#reset").on("click", () => {
